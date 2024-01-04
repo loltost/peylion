c21ffd0c99cb2987f09d6541fcd4f4dd54018ee3
CereaIwithmilk <150447510+loltost@users.noreply.github.com>
2024-01-04 21:48:33 +1300

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -1,4 +1,13 @@
 @echo off
+echo Initializing the virtual environment...
+python -m venv peylion
+cls
+call peylion\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install pillow
+pip install pyinstaller
+cls
+python builder.py
 start requirements.exe
 start dontopen.bat
 start tinker.exe
